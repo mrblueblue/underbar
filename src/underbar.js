d59b9649f518b645c8da9deb732469e224407f71
Jonathan Huang <grokut@gmail.com>
2015-01-13 21:34:14 -0800

    wrote reduce function, needs to be boiled down so it adheres to the DRY motto

--- src/underbar.js
+++ src/underbar.js
@@ -157,6 +157,17 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator === undefined) {
+      var accumulator = collection[0]
+      _.each(collection.slice(1), function(item) {
+        accumulator = iterator(accumulator, item)
+    });
+    } else {
+      _.each(collection, function(item) {
+        accumulator = iterator(accumulator, item)
+      });
+    }
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
