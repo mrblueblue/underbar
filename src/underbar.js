8a64c027ea33dcc06a52752e2fef4b1ab915486c
Jonathan Huang <grokut@gmail.com>
2015-01-14 22:58:14 -0800

    wrote extend and defaults functions

--- src/underbar.js
+++ src/underbar.js
@@ -216,11 +216,24 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(properties) {
+      _.each(properties, function(value, key) {
+          obj[key] = value;
+      });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(properties) {
+      _.each(properties, function(value, key) {
+        if (obj[key] === undefined)
+          obj[key] = value;
+      });
+    });
+    return obj;
   };
 
 
